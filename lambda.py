09b5c87f5ff69b747d3078b9187a8055eb8693bd
Rasmi Elasmar <rrelasmar@gmail.com>
2015-12-12 00:51:59 -0500

    First commit.

---
+++ lambda.py
@@ -0,0 +1,110 @@
+from __future__ import print_function
+import re
+import urllib2
+
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
+    print("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
+
+    """
+    Uncomment this if statement and populate with your skill's application ID to
+    prevent someone else from configuring a skill that sends requests to this
+    function.
+    """
+    # if (event['session']['application']['applicationId'] !=
+    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
+    #     raise ValueError("Invalid Application ID")
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
+
+
+def on_session_started(session_started_request, session):
+    """ Called when the session starts """
+
+    print("on_session_started requestId=" + session_started_request['requestId']
+          + ", sessionId=" + session['sessionId'])
+
+def on_launch(launch_request, session):
+    """ Called when the user launches the skill without specifying what they
+    want
+    """
+
+    print("on_launch requestId=" + launch_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # Dispatch to your skill's launch
+    return explanation_response()
+
+
+def on_intent(intent_request, session):
+    """ Called when the user specifies an intent for this skill """
+
+    print("on_intent requestId=" + intent_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+    #intent = intent_request['intent']
+    #intent_name = intent_request['intent']['name']
+    
+    return explanation_response()
+
+def on_session_ended(session_ended_request, session):
+    """ Called when the user ends the session.
+
+    Is not called when the skill returns should_end_session=true
+    """
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+# --------------- Functions that control the skill's behavior ------------------
+
+def get_explanation():
+    url = 'http://apod.nasa.gov/apod/astropix.html'
+    request = urllib2.urlopen(url)
+    html = request.read()
+    html = html.replace('\n',' ')
+    explanation_html = html.split('<b> Explanation: </b>')[1]
+    explanation_html = explanation_html.split("<b> Tomorrow\'s picture: </b>")[0]
+    explanation = re.sub(r'<.*?>','', explanation_html)
+    explanation = re.sub(r'\s+',' ', explanation)
+
+    return explanation
+
+def explanation_response():
+    session_attributes = {}
+    title = 'APOD'
+    output = get_explanation()
+    return build_response(session_attributes, build_speechlet_response(title, output))
+
+# ------------------ Helper functions to build the response --------------------
+
+def build_speechlet_response(title, output):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': title,
+            'content': output
+        },
+        'shouldEndSession': True
+    }
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
